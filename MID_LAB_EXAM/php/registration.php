bba9b41e394f83378eef4a9a30c7cbd2411ebc59
Zubair <you@example.com>
2020-11-10 10:06:08 +0600

    commit at 10 AM

---
+++ MID_LAB_EXAM/php/registration.php
@@ -0,0 +1,30 @@
+<?php  
+if(isset($_POST["submit"])){  
+if(!empty($_POST['user']) && !empty($_POST['pass'])) {  
+    $user=$_POST['user'];  
+    $pass=$_POST['pass'];  
+    $con=mysql_connect('localhost','root','') or die(mysql_error());  
+    mysql_select_db('user_registration') or die("cannot select DB");  
+  
+    $query=mysql_query("SELECT * FROM login WHERE username='".$user."'");  
+    $numrows=mysql_num_rows($query);  
+    if($numrows==0)  
+    {  
+    $sql="INSERT INTO login(username,password) VALUES('$user','$pass')";  
+  
+    $result=mysql_query($sql);  
+        if($result){  
+    echo "Account Successfully Created";  
+    } else {  
+    echo "Failure!";  
+    }  
+  
+    } else {  
+    echo "That username already exists! Please try again with another.";  
+    }  
+  
+} else {  
+    echo "All fields are required!";  
+}  
+}  
+?>  
