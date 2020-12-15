7d6aecf3d23eb66ebd255c2e515718f686c481d8
Zubair <you@example.com>
2020-12-15 10:48:19 +0600

    commit

---
+++ FINAL_LAB_TASK_4.1_AJAX/abc.php
@@ -0,0 +1,20 @@
+  
+<?php
+
+	//sleep(3);
+
+	$term  = $_POST['search'];
+
+	$data = "<table border=1>
+				<tr>
+					<td>ID</td>
+					<td>Name</td>
+					<td>Email</td>
+				</tr>
+				<tr>
+					<td>1</td>
+					<td>{$term}</td>
+					<td>{$term}@aiub.edu</td>
+				</tr>";
+	echo $data;
+?>
